fb8fa484b8ebeef327be2578be4cc8d2b635ec2f
MacGyverOfCode <richard_a_roth@yahoo.com>
2016-02-03 17:36:59 +0000

    Removed parseMonth function

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
 function getXHR(url) {
 	return new Promise(function(resolve, reject) {
 		var req = new XMLHttpRequest();
@@ -17,38 +19,9 @@ function getXHR(url) {
 };
 
 function parseDate(itemDate) {
-	itemDate = itemDate.split('T');
-	itemDate = itemDate[0].split('-');
-	itemDate = parseMonth(itemDate[1]) + ' ' + parseInt(itemDate[2]) + ', ' + itemDate[0];
+	itemDate = itemDate.split('T')[0].split('-');
+	itemDate = months[parseInt(itemDate[1], 10) - 1] + ' ' + parseInt(itemDate[2]) + ', ' + itemDate[0];
 	return itemDate;
-};
-
-function parseMonth(month) {
-	if (month == '01') {
-		return 'January';
-	} else if (month == '02') {
-		return 'February';
-	} else if (month == '03') {
-		return 'March';
-	} else if (month == '04') {
-		return 'April';
-	} else if (month == '05') {
-		return 'May';
-	} else if (month == '06') {
-		return 'June';
-	} else if (month == '07') {
-		return 'July';
-	} else if (month == '08') {
-		return 'August';
-	} else if (month == '09') {
-		return 'September';
-	} else if (month == '10') {
-		return 'October';
-	} else if (month == '11') {
-		return 'November';
-	} else if (month == '12') {
-		return 'December';
-	};
 };
 
 function parseURL(linkURL) {
